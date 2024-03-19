51868851fb84790ba5dc0b45d579b2bdf87de040
HowardM12138 <mengbingxu@gmail.com>
2024-03-18 19:53:51 -0700

    Finished button by creating primary and secondary mode

--- broadth.ink_frontend_react_app/src/components/Common/Button.js
+++ broadth.ink_frontend_react_app/src/components/Common/Button.js
@@ -3,12 +3,16 @@ import Button from "@mui/material/Button"; // Import Button component from Mater
 
 /*
 
-Button to be used everywhere on the website
+Button to be used everywhere on the website. 
+Primary has color purple, and secondary has color gray.
 
 Example usage:
   import MyButtonComponent from "./components/Common/Button.js";
 
   <MyButtonComponent
+    mode="primary"
+    text="Click Me!"
+    onclick={Your Function Here}
     height='60px'
     width='200px'
     fontSize='25px'
@@ -21,26 +25,40 @@ Everything that's about style has been set.
 
 */
 
-function MyButtonComponent({ height, width, fontSize, borderRadius }) {
+function MyButtonComponent({
+	mode,
+	text,
+	onClick,
+	height,
+	width,
+	fontSize,
+	borderRadius,
+}) {
+	const primaryColor = "#9172EC";
+	const secondaryColor = "#EDEAEA";
+
 	return (
 		<div>
 			{/* Use the Button component from Material UI */}
 			<Button
 				variant='contained'
+				onClick={onClick}
 				sx={{
-					backgroundColor: "#9172EC", // Set custom background color
+					backgroundColor: mode === "secondary" ? secondaryColor : primaryColor,
 					"&:hover": {
-						backgroundColor: "#9172EC", // Darker shade for hover state
+						backgroundColor:
+							mode === "secondary" ? secondaryColor : primaryColor,
 					},
 					fontFamily: "Open Sans", // Set font family to Open Sans
-					fontWeight: "800", // Extra bold
+					fontWeight: mode === "secondary" ? "400" : "700", // Extra bold for primary
+					color: mode === "secondary" ? "black" : "white",
 					textTransform: "none", // Keep the text in its original case
 					borderRadius: borderRadius, // Setting the borderRadius size
-					fontSize: fontSize, // Set font size to 25px
+					fontSize: fontSize, // Set font size
 					height: height, // Use height prop for button height
 					width: width, // Use width prop for button width
 				}}>
-				Click Me
+				{text} {/* Display custom text */}
 			</Button>
 		</div>
 	);
